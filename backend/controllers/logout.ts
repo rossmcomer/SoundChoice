930631548f53793ae6619aa3008a1e3bd7487ae8
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-10 23:39:12 -0500

    add logout endpoint

---
+++ backend/controllers/logout.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+const router = require('express').Router();
+
+router.post('/', async (_req: Request, res: Response): Promise<Response> => {
+    
+  return res.status(200).json({ message: 'Logged out successfully' });
+});
+
+module.exports = router;
