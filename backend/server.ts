d8695dea5888c1776d1e3a2373afd3a51def69fa
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-11 12:19:28 -0500

    add logout and bookings routers to server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -6,12 +6,18 @@ const { connectToDatabase } = require ('./util/db.ts');
 
 const usersRouter = require('./controllers/users')
 const availabilityRouter = require('./controllers/availability')
+const loginRouter = require('./controllers/login')
+const logoutRouter = require('./controllers/logout')
+const bookingsRouter = require('./controllers/bookings')
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', usersRouter)
 app.use('/api/availability', availabilityRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/logout', logoutRouter)
+app.use('/api/booking', bookingsRouter)
 
 const start = async () => {
     await connectToDatabase();
