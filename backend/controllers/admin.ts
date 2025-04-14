4915f0611dfbfcec81df2fcb12bd7610377713e3
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-14 12:30:16 -0500

    add admin endpoint to get all booking info for a specific bookingId

--- backend/controllers/admin.ts
+++ backend/controllers/admin.ts
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 import { Request, Response } from 'express';
-import { User } from '../types';
 import { prisma } from '../util/db';
 const { tokenExtractor, requireAdmin } = require('../util/middleware');
 
@@ -36,3 +35,39 @@ router.get(
       }
     }
 );
+
+// GET info about a specific bookingId
+router.get(
+    '/booking-info/:bookingId',
+    tokenExtractor,
+    requireAdmin, // Ensure the user is an admin before proceeding
+    async (req: Request, res: Response): Promise<Response> => {
+      const { bookingId } = req.params; // Get the bookingId from the URL parameter
+  
+      try {
+        // Fetch the booking using bookingId
+        const booking = await prisma.booking.findUnique({
+          where: {
+            bookingId: bookingId, // Find user by their email
+          },
+          include: {
+            payment: true,
+            questionnaire: true,
+          },
+        });
+  
+        // If the booking is not found, return a 404 error
+        if (!booking) {
+          return res.status(404).json({ error: 'Booking not found' });
+        }
+  
+        // Return the booking data along with their related information
+        return res.status(200).json(booking);
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Failed to retrieve booking data' });
+      }
+    }
+  );
+
+  module.exports = router;
