92e8419af5df9ef623eb080cbbe35dae7ef7bade
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-14 21:35:52 -0500

    add additional types into types file

--- backend/generated/prisma/edge.js
+++ backend/generated/prisma/edge.js
@@ -225,11 +225,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:password@localhost:5432/iCite-db"
+        "value": null
       }
     }
   },
