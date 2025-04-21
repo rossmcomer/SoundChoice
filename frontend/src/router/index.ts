9d14e10ac093bccc65b5f1863fd304753dba16aa
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-21 15:57:35 -0500

    add routes to router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '../views/AboutView.vue';
+import PricingView from '../views/PricingView.vue';
+import BookingView from '../views/BookingView.vue';
+import TestimonialsView from '../views/TestimonialsView.vue';
+import MediaView from '../views/MediaView.vue';
+import LoginView from '../views/LoginView.vue';
+import LogoutView from '../views/LogoutView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +20,36 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/pricing',
+      name: 'pricing',
+      component: PricingView,
+    },
+    {
+      path: '/booking',
+      name: 'booking',
+      component: BookingView,
+    },
+    {
+      path: '/testimonials',
+      name: 'testimonials',
+      component: TestimonialsView,
+    },
+    {
+      path: '/media',
+      name: 'media',
+      component: MediaView,
+    },
+    {
+      path: '/login-sign-up',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: LogoutView,
     },
   ],
 });
