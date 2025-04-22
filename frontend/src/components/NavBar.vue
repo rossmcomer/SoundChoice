a3f8f123ba9b4bdb35bcd0076232259956b0af65
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-21 22:49:07 -0500

    add more styling to navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,35 +3,37 @@ import { ref, computed } from 'vue'
 import SCLogoPhones from '@/assets/SoundChoice-Logo/SC-Logo-Small.png'
 import SCLogoText from '@/assets/SoundChoice-Logo/SC-Logo-Text-Only.png'
 import { useUserStore } from '@/stores/UserStore'
+import { useRouter } from 'vue-router'
 
 const isOpen = ref(false)
 const toggleMenu = () => {
   isOpen.value = !isOpen.value
 }
 
+const router = useRouter()
 const userStore = useUserStore()
 const user = computed(() => userStore.user)
 </script>
 
 <template>
-  <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+  <nav class="fixed w-full z-20 top-0 start-0">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <router-link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img :src="SCLogoPhones" class="h-8" alt="SoundChoice Logo" />
         <img :src="SCLogoText" class="h-8" alt="SoundChoice Logo" />
       </router-link>
-
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <button
           type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          @click="$router.push('/booking')"
+          class="bookNowBtn  focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center"
         >
           Book Now!
         </button>
         <button
           @click="toggleMenu"
           type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          class="toggleMenu inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="navbar-sticky"
           aria-expanded="false"
         >
@@ -41,66 +43,43 @@ const user = computed(() => userStore.user)
           </svg>
         </button>
       </div>
-
       <div
         :class="['items-center justify-between w-full md:flex md:w-auto md:order-1', isOpen ? 'flex' : 'hidden']"
         id="navbar-sticky"
       >
-        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
           <li>
-            <router-link
-              to="/"
-              class="block py-2 px-3 text-white rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-              aria-current="page"
-            >
+            <router-link to="/" class="block py-2 px-3 text-white rounded-sm nav-link-md" aria-current="page">
               Home
             </router-link>
           </li>
           <li>
-            <router-link
-              to="/about"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
+            <router-link to="/about" class="block py-2 px-3 nav-link-md">
               About
             </router-link>
           </li>
           <li>
-            <router-link
-              to="/pricing"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
+            <router-link to="/pricing" class="block py-2 px-3 nav-link-md">
               Pricing
             </router-link>
           </li>
           <li>
-            <router-link
-              to="/testimonials"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
+            <router-link to="/testimonials" class="block py-2 px-3 nav-link-md">
               Testimonials
             </router-link>
           </li>
           <li>
-            <router-link
-              to="/media"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
+            <router-link to="/media" class="block py-2 px-3 nav-link-md">
               Media
             </router-link>
           </li>
           <li v-if="!user">
-            <router-link
-              to="/login"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
+            <router-link to="/login" class="block py-2 px-3 nav-link-md">
               Login / Signup
             </router-link>
           </li>
           <li v-else>
-            <router-link
-              to="/logout"
-              class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
+            <router-link to="/logout" class="block py-2 px-3 nav-link-md">
               Logout
             </router-link>
           </li>
@@ -111,19 +90,59 @@ const user = computed(() => userStore.user)
 </template>
 
 <style scoped>
+@media (hover: hover) {
+  .nav-link-md:hover {
+    background-color: transparent !important;
+    color: var(--color6) !important;
+    transform: scale(1.25);
+  }
+
+  .bookNowBtn:hover {
+  background-color: var(--grey-soft);
+  box-shadow: 0 0 0 2px var(--color6);
+  color: var(--black-soft);
+  cursor: pointer;
+}
+
+.toggleMenu:hover {
+  color: var(--color6);
+  cursor: pointer;
+}
+}
+
+@media (min-width: 768px) {
+  .nav-link-md {
+    padding: 0 !important;
+    background-color: transparent !important;
+  }
+
+  
+.bookNowBtn:focus {
+  
+  outline: 2px solid transparent;
+  outline-offset: 2px;
+  box-shadow: 0 0 0 1px var(--color6);
+}
+}
 
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
+  color: var(--white-soft);
   background-color: var(--black-soft) !important;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+  color: var(--color6);
 }
 
 nav a.router-link-exact-active:hover {
   background-color: transparent;
+}
+
+.bookNowBtn {
+  background-color: var(--color6);
+  color: var(--black-soft)
 }
 </style>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,21 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}',
-    "./node_modules/flowbite/**/*.js"],
+    './node_modules/flowbite/**/*.js'],
     theme: {
       extend: {
         colors: {
-        color1: 'var(--color1)',
-        color2: 'var(--color2)',
-        color3: 'var(--color3)',
-        color4: 'var(--color4)',
-        color5: 'var(--color5)',
-        color6: 'var(--color6)',
-        'white-soft': 'var(--white-soft)',
-        'black-soft': 'var(--black-soft)',
-        'grey-soft': 'var(--grey-soft)',
-        'purple-royal': 'var(--purple-royal)',
-      }},
+          color1: 'var(--color1)',
+          color2: 'var(--color2)',
+          color3: 'var(--color3)',
+          color4: 'var(--color4)',
+          color5: 'var(--color5)',
+          color6: 'var(--color6)',
+          'white-soft': 'var(--white-soft)',
+          'black-soft': 'var(--black-soft)',
+          'grey-soft': 'var(--grey-soft)',
+          'purple-royal': 'var(--purple-royal)',
+        },
+    },
     },
     plugins: [require('flowbite/plugin')],
   };
