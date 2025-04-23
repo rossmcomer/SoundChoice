ad3bee303d7d8f5d904bccf1fef13808fdf81251
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-22 21:20:35 -0500

    modify background transition to remove scrollbars from popping up

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,18 +2,26 @@
 
 </script>
 <template>
-  <div class="homeContainer h-full">
-    <div class="h-full flex flex-col place-items-center justify-center">
-      <video class="w-full h-auto max-w-2xl rounded-lg shadow-lg mb-8" autoplay muted controls>
-        <source src="@/assets/Promo-video.mp4" type="video/mp4">
-      Your browser does not support the video tag.
+  <div class="relative h-full overflow-hidden">
+    <div class="absolute inset-0 bg-cover bg-center scaleAnimation"></div>
+
+    <div class="h-full flex flex-col place-items-center justify-start pt-20 relative z-10">
+      <video
+        class="w-4/5 h-auto max-w-3xl rounded-lg shadow-lg mb-8 border-2 border-yellow-600"
+        autoplay
+        muted
+        controls
+      >
+        <source src="@/assets/Promo-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
     </div>
   </div>
 </template>
 <style scoped>
-.homeContainer {
+.scaleAnimation {
   background-image: url('../assets/SoundChoice-Logo/SoundChoice-DJ-Services.svg');
+  z-index: -1;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
