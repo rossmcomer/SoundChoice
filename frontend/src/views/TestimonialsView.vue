53d23e3f8720659dd2debbf76d426c855726c70e
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-24 13:30:51 -0500

    complete testimonials carousel first draft

--- frontend/src/views/TestimonialsView.vue
+++ frontend/src/views/TestimonialsView.vue
@@ -7,45 +7,10 @@ import { testimonials } from '@/assets/testimonials';
       <source src="@/assets/movies/movie2-loop.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
-    <div class="relative z-10 pt-10 justify-items-center">
+    <div class="relative z-10 pt-10">
       <div class="flex justify-center py-7">
         <h1 class="text-6xl">Testimonials</h1>
       </div>
-      <!-- <div
-        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center p-4 sm:p-10"
-      >
-        <div
-          v-for="(testimonial, index) in testimonials"
-          :key="index"
-          class="flex flex-col justify-between p-6 border-[rgb(34,34,34)] border-2 rounded-xl shadow-lg w-full max-w-[500px] min-h-[300px] bg-gradient-to-b from-[rgba(136,136,136,0.3)] to-transparent"
-        >
-          <div class="flex items-center mb-4">
-            <div class="flex items-center space-x-1">
-              <svg
-                v-for="n in Math.floor(testimonial.stars)"
-                :key="n"
-                class="w-5 h-5 text-yellow-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.14 3.513a1 1 0 00.95.69h3.688c.969 0 1.371 1.24.588 1.81l-2.985 2.17a1 1 0 00-.364 1.118l1.14 3.513c.3.921-.755 1.688-1.538 1.118l-2.985-2.17a1 1 0 00-1.176 0l-2.985 2.17c-.783.57-1.838-.197-1.538-1.118l1.14-3.513a1 1 0 00-.364-1.118L2.69 8.94c-.783-.57-.38-1.81.588-1.81h3.688a1 1 0 00.95-.69l1.14-3.513z"
-                ></path>
-              </svg>
-            </div>
-            <span class="ml-2 text-sm font-bold" style="color: var(--black-soft)">
-              "{{ testimonial.starsLabel }}"
-            </span>
-          </div>
-          <div class="flex-1">
-            <p class="text-gray-800 mb-4 font-medium">{{ testimonial.message }}</p>
-          </div>
-          <div class="border-t border-gray-400 pt-2">
-            <p class="text-sm text-gray-700">- {{ testimonial.author }}</p>
-          </div>
-        </div>
-      </div> -->
       <div class="swiperWrapper">
       <swiper-container
     class="mySwiper pt-4 sm:py-10"
@@ -92,7 +57,7 @@ import { testimonials } from '@/assets/testimonials';
             </span>
           </div>
           <div class="flex-1">
-            <p class="text-gray-800 mb-4 font-medium">{{ testimonial.message }}</p>
+            <p class="text-gray-800 mb-4 font-medium h-[275px]">{{ testimonial.message }}</p>
           </div>
           <div class="border-t border-gray-400 pt-2">
             <p class="text-sm text-gray-700">- {{ testimonial.author }}</p>
@@ -107,4 +72,9 @@ import { testimonials } from '@/assets/testimonials';
 swiper-container {
   width: 100%;
 }
+
+/* swiper-slide.swiper-slide-next,
+swiper-slide.swiper-slide-prev {
+  align-self: center;
+} */
 </style>
