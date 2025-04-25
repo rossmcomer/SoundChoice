70bb51243b6bff986a1ffbba1af1b86ee0d4fd6d
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-24 23:53:24 -0500

    add barebones elements needed for booking

--- frontend/src/views/BookingView.vue
+++ frontend/src/views/BookingView.vue
@@ -52,6 +52,18 @@ onMounted(() => {
     auto-apply
     dark
   ></VueDatePicker>
+
+  <form class="max-w-sm mx-auto">
+  <label for="type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+  <select id="eventType" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <option selected>Choose Event Type</option>
+    <option value="wedding">Wedding</option>
+    <option value="corporate">Corporate</option>
+    <option value="birthday">Birthday</option>
+    <option value="barBatMitzvah">Mitzvah</option>
+    <option value="other">Other</option>
+  </select>
+</form>
   
 <form class="max-w-[16rem] mx-auto grid grid-cols-2 gap-4">
     <div>
@@ -77,6 +89,32 @@ onMounted(() => {
         </div>
     </div>
 </form>
+
+<h3 class="mb-5 text-lg font-medium text-gray-900 dark:text-white">Would you like to add the additional uplighting package for only $200 extra?</h3>
+<ul class="grid w-full gap-6 md:grid-cols-2">
+    <li>
+        <input type="radio" id="hosting-small" name="hosting" value="hosting-small" class="hidden peer" required />
+        <label for="hosting-small" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">                           
+            <div class="block">
+                <div class="w-full text-lg font-semibold">Yes</div>
+            </div>
+            <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </label>
+    </li>
+    <li>
+        <input type="radio" id="hosting-big" name="hosting" value="hosting-big" class="hidden peer">
+        <label for="hosting-big" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div class="block">
+                <div class="w-full text-lg font-semibold">No</div>
+            </div>
+            <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </label>
+    </li>
+</ul>
 
 </template>
 <style>
