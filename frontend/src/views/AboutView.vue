d8553927997ec711e483a307801c1b29e9728b6e
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-04-29 13:05:49 -0500

    add border to about paragraph

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -21,7 +21,7 @@
             />
           </div>
           <div
-            class="flex-1 font-medium p-8 space-y-4 rounded-xl shadow-lg text-gray-800 min-h-[250px] bg-gradient-to-b from-[rgba(136,136,136,0.3)] to-transparent"
+            class="flex-1 font-medium p-8 space-y-4 border-2 border-[rgb(34,34,34)] rounded-xl shadow-lg text-gray-800 min-h-[250px] bg-gradient-to-b from-[rgba(136,136,136,0.3)] to-transparent"
           >
             <p>
               &nbsp;&nbsp;&nbsp;&nbsp;SoundChoice founder/operator Kyle Beller has been DJing
