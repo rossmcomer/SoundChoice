a8c0b6ed6e4f3cc418ef3392168829e0dd65257c
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-05-01 15:19:13 -0500

    add styling to uplighting selector

--- frontend/src/views/BookingView.vue
+++ frontend/src/views/BookingView.vue
@@ -7,6 +7,7 @@ import { useAvailabilityStore } from '@/stores/AvailabilityStore';
 import BookingInstructionsCard from '@/components/BookingInstructionsCards.vue';
 import EventTypeDropdown from '@/components/EventTypeDropdown.vue';
 import EventTimeSelector from '@/components/EventTimeSelector.vue';
+import UplightingSelector from '@/components/UplightingSelector.vue';
 
 const userStore = useUserStore();
 const availabilityStore = useAvailabilityStore();
@@ -17,6 +18,7 @@ const date = ref<Date | null>(null);
 const eventType = ref<string>('');
 const startTime = ref<string>('00:00');
 const endTime = ref<string>('00:00');
+const uplights = ref<boolean>(false)
 
 const today = new Date();
 
@@ -57,77 +59,8 @@ onMounted(() => {
 
         <EventTypeDropdown v-model="eventType" />
         <EventTimeSelector v-model:startTime="startTime" v-model:endTime="endTime" />
-
-        <h3 class="mb-5 text-lg font-medium text-gray-900 dark:text-white">
-          Would you like to add the additional uplighting package for only $200 extra?
-        </h3>
-        <ul class="grid w-full gap-6 md:grid-cols-2">
-          <li>
-            <input
-              type="radio"
-              id="hosting-small"
-              name="hosting"
-              value="hosting-small"
-              class="hidden peer"
-              required
-            />
-            <label
-              for="hosting-small"
-              class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-              <div class="block">
-                <div class="w-full text-lg font-semibold">Yes</div>
-              </div>
-              <svg
-                class="w-5 h-5 ms-3 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              id="hosting-big"
-              name="hosting"
-              value="hosting-big"
-              class="hidden peer"
-            />
-            <label
-              for="hosting-big"
-              class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-              <div class="block">
-                <div class="w-full text-lg font-semibold">No</div>
-              </div>
-              <svg
-                class="w-5 h-5 ms-3 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </label>
-          </li>
-        </ul>
+        <UplightingSelector v-model="uplights" />
+        
       </div>
     </div>
   </div>
