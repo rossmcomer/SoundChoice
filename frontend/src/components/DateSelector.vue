1d74bad59a4de31cd969d6d160bf74c0410bc66f
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-05-01 20:16:00 -0500

    refactor date selector into its own component

---
+++ frontend/src/components/DateSelector.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import '@vuepic/vue-datepicker/dist/main.css';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import { onMounted } from 'vue';
+import { useAvailabilityStore } from '@/stores/AvailabilityStore';
+
+const availabilityStore = useAvailabilityStore();
+
+const today = new Date();
+const date = defineModel<string>('startTime');
+
+onMounted(() => {
+  availabilityStore.fetchDates();
+});
+</script>
+<template>
+    <div class="sm:text-2xl text-xl text-center font-bold text-gray-800 pt-10">
+          Pick A Day For Your Event!
+        </div>
+        <div class="sm:text-sm text-center pb-4 text-gray-800">
+          Date not available? Please contact us!
+        </div>
+        <div class="flex justify-center">
+          <VueDatePicker
+            v-model="date"
+            :min-date="today"
+            :disabled-dates="availabilityStore.unavailableDates.map((d) => new Date(d.date))"
+            inline
+            auto-apply
+            dark
+            :enable-time-picker="false"
+          ></VueDatePicker>
+        </div>
+</template>
+<style>
+.dp__theme_dark {
+  --dp-primary-color: #daa520;
+  --dp-primary-text-color: var(--black-soft);
+  --dp-background-color: var(--black-soft);
+  --dp-text-color: var(--white-soft);
+}
+</style>
--- frontend/src/views/BookingView.vue
+++ frontend/src/views/BookingView.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import '@vuepic/vue-datepicker/dist/main.css';
-import VueDatePicker from '@vuepic/vue-datepicker';
 import { ref, computed, onMounted } from 'vue';
 import type { EventType } from '@/types';
 import { useUserStore } from '@/stores/UserStore';
-import { useAvailabilityStore } from '@/stores/AvailabilityStore';
 import BookingInstructionsCard from '@/components/BookingInstructionsCards.vue';
+import DateSelector from '@/components/DateSelector.vue';
 import EventTypeDropdown from '@/components/EventTypeDropdown.vue';
 import EventTimeSelector from '@/components/EventTimeSelector.vue';
 import UplightingSelector from '@/components/UplightingSelector.vue';
 import AdditionalHours from '@/components/AdditionalHours.vue';
 
 const userStore = useUserStore();
-const availabilityStore = useAvailabilityStore();
 
 const user = computed(() => userStore.user);
 
@@ -23,16 +20,10 @@ const endTime = ref<string>('00:00');
 const uplights = ref<boolean>(false);
 const addHours = ref<boolean>(false);
 const additionalHours = ref<number>(1);
-
-const today = new Date();
-
-onMounted(() => {
-  availabilityStore.fetchDates();
-});
 </script>
 
 <template>
-  <div v-if="!user" class="relative h-full">
+  <div v-if="user" class="relative h-full">
     <video class="fixed inset-0 w-full h-full object-cover z-0" autoplay muted loop playsinline>
       <source src="@/assets/movies/movie2-loop.mp4" type="video/mp4" />
       Your browser does not support the video tag.
@@ -80,23 +71,7 @@ onMounted(() => {
       </div>
       <div class="flex flex-col items-center p-4 sm:p-10">
         <BookingInstructionsCard />
-        <div class="sm:text-2xl text-xl text-center font-bold text-gray-800 pt-10">
-          Pick A Day For Your Event!
-        </div>
-        <div class="sm:text-sm text-center pb-4 text-gray-800">
-          Date not available? Please contact us!
-        </div>
-        <div class="flex justify-center">
-          <VueDatePicker
-            v-model="date"
-            :min-date="today"
-            :disabled-dates="availabilityStore.unavailableDates.map((d) => new Date(d.date))"
-            inline
-            auto-apply
-            dark
-            :enable-time-picker="false"
-          ></VueDatePicker>
-        </div>
+        <DateSelector v-model="date"/>
 
         <EventTypeDropdown v-model="eventType" />
         <EventTimeSelector v-model:startTime="startTime" v-model:endTime="endTime" />
@@ -115,10 +90,4 @@ onMounted(() => {
   </div>
 </template>
 <style>
-.dp__theme_dark {
-  --dp-primary-color: #daa520;
-  --dp-primary-text-color: var(--black-soft);
-  --dp-background-color: var(--black-soft);
-  --dp-text-color: var(--white-soft);
-}
 </style>
