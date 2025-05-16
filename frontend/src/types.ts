0cc6e66557907cb7b00d4b55b0977e51339ac1b1
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-05-16 14:09:27 -0500

    update PaymentMethod model to include cash paymentMethod

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -64,7 +64,7 @@ export type PaymentStatus =
   | 'remainingPaymentFailed'
   | 'paidInFull';
 
-export type PaymentMethod = 'stripe' | 'paypal';
+export type PaymentMethod = 'stripe' | 'paypal' | 'cash';
 
 export interface ProductItem {
   id: number;
