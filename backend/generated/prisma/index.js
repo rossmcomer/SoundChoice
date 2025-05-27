57a6f06f778047c99c0803babc647551a6d233dc
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2025-05-27 16:16:02 -0500

    seed testimonials in database with prisma

--- backend/generated/prisma/index.js
+++ backend/generated/prisma/index.js
@@ -242,11 +242,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:password@localhost:5432/iCite-db"
+        "value": null
       }
     }
   },
